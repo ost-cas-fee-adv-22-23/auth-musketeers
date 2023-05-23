5f704cf47a5b09ae3bd5da90e7627f47ae9e4aeb
André Abt <info@andre-abt.com>
2023-05-23 10:35:06 +0200

    feat: #0 Nicer styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import { signIn, signOut, useSession } from "next-auth/react";
 import logger from "@/services/logger";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,10 @@ export default function Home() {
   logger.info({ session: session }, "index.tsx session");
 
   return (
-    <>
+    <div className="h-auto text-center border border-1 p-5">
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        Musketeer&apos;s Auth Sandbox App
+      </h1>
       {!session && (
         <>
           <p>Not signed in</p>
@@ -25,6 +29,7 @@ export default function Home() {
                 callbackUrl: "http://localhost:3000/",
               })
             }
+            className="border border-1 p-2"
           >
             Sign in
           </button>
@@ -33,22 +38,37 @@ export default function Home() {
       {loading && <span>loading...</span>}
       {session && (
         <>
-          <p>
-            Signed in as {session.user?.loginName} {session.user?.email}
-            <Image
-              src={session.user?.image ?? ""}
-              alt="Profile picture"
-              width={100}
-              height={100}
-              className="w-auto h-auto"
-            />
-          </p>
-          <button onClick={() => signOut()}>Sign out</button>
+          <div className="">
+            <p className="text-center">
+              Signed in as {session.user?.loginName} {session.user?.email}{" "}
+              <br />
+              <Image
+                src={session.user?.image ?? ""}
+                alt="Profile picture"
+                width={100}
+                height={100}
+                className="mt-5 mb-5 w-auto h-auto inline-block"
+              />
+            </p>
+            <button onClick={() => signOut()} className="border border-1 p-2">
+              Sign out
+            </button>
 
-          <h3 className="mt-10">session</h3>
-          <pre>{JSON.stringify(session, null, 2)}</pre>
+            <div className="bg-gray-900 p-5 mt-10 mb-10">
+              <h3>Debug Info (Please also open the console)</h3>
+              <pre className="text-left w-50">
+                session:
+                {JSON.stringify(session, null, 2)}
+              </pre>
+            </div>
+          </div>
+          <div>
+            <Link href="/profile" className="border border-1 p-2 mt-10">
+              Goto Profile Page
+            </Link>
+          </div>
         </>
       )}
-    </>
+    </div>
   );
 }
